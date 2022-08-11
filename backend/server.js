6a7d78181e75b985c5cd445d9aba528e6df31d0a
kamhar-ngado <kamhar.ngado.id@gmail.com>
2022-08-11 22:47:44 +0800

    fix file location

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,23 @@
-require('dotenv').config()
+require("dotenv").config();
 
-const express = require('express')
-const workoutRoutes = require('./routes/workouts')
+const express = require("express");
+const workoutRoutes = require("./routes/workouts.js");
 
 // express app
-const app = express()
+const app = express();
 
 // middleware
-app.use(express.json())
+app.use(express.json());
 
 app.use((req, res, next) => {
-  console.log(req.path, req.method)
-  next()
-})
+  console.log(req.path, req.method);
+  next();
+});
 
 // routes
-app.use('/api/workouts', workoutRoutes)
+app.use("/api/workouts", workoutRoutes);
 
 // listen for requests
 app.listen(process.env.PORT, () => {
-  console.log('listening on port', process.env.PORT)
-})
+  console.log("listening on port", process.env.PORT);
+});
